6529420921162ca713e2da6bbe50d54a1cd4ca59
C2H5OH <n.f.mikhaylov@gmail.com>
2024-09-15 00:16:47 +0300

    Начата работа над 3 лабой

---
+++ LAB №3/Application/enums.h
@@ -0,0 +1,38 @@
+#pragma once
+enum keys
+{
+    KEY_UP = 72,
+    KEY_DOWN = 80,
+    KEY_LEFT = 75,
+    KEY_RIGHT = 77,
+    ESC = 27,
+    ENTER = 13,
+
+    ZERO = 48,
+    ONE = 49,
+    TWO = 50,
+    THREE = 51,
+    FOUR = 52,
+    FIVE = 53,
+    SIX = 54,
+    SEVEN = 55,
+};
+
+enum menuConst {
+    MAIN_MENU_HIGHT = 6,
+    SORT_MENU_HIGHT = 2,
+
+    MAIN_MENU = -1,
+
+    CREATE_ARRAY = 0,
+    SKO = 1,
+    SORT = 2,
+    CHANGE_ARRAY_SIZE = 3,
+    CHANGE_ITEM_VALUE = 4,
+    PRINT_ARRAY = 5,
+    EXIT = 6,
+
+    SORT_ASCENDING = 0,
+    SORT_DESCENDING = 1,
+    SORT_EXIT = 2
+};
